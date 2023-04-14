46b61e77eba7d738cf1a1654996e9678f0b7f7c7
tonycava <anthony.cavagne@ynov.com>
2023-04-14 17:27:10 +0200

    add: dist

---
+++ dist/index.d.ts
@@ -0,0 +1,37 @@
+export declare type Chat = {
+    id: string;
+    userId: string;
+    content: string;
+    createdAt: Date;
+    user: {
+        username: string;
+    };
+};
+export declare enum WEB_SOCKET_EVENT {
+    CONNECT = "connect",
+    LEAVE_WAITING = "leaveWaiting",
+    JOIN_GAME = "joinGame",
+    JOIN_WAITING = "joinWaiting",
+    UPDATE = "update",
+    PLAY = "play",
+    CHAT = "chat",
+    NEW_MESSAGE = "newMessage",
+    PARTNER = "partner"
+}
+export declare type Range = {
+    start: number;
+    end: number;
+};
+export declare type Game = {
+    state: 'choosing' | 'reveal' | 'finished';
+    round: number;
+    timerPlay: number;
+    timerRev: number;
+    players: [string, string];
+    whoWin: [Some, Some, Some];
+    actualPlay: {
+        [player: string]: string | Move;
+    };
+};
+export declare type Some = string | null;
+export declare type Move = 'rock' | 'paper' | 'scissors';
---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WEB_SOCKET_EVENT = void 0;
+var WEB_SOCKET_EVENT;
+(function (WEB_SOCKET_EVENT) {
+    WEB_SOCKET_EVENT["CONNECT"] = "connect";
+    WEB_SOCKET_EVENT["LEAVE_WAITING"] = "leaveWaiting";
+    WEB_SOCKET_EVENT["JOIN_GAME"] = "joinGame";
+    WEB_SOCKET_EVENT["JOIN_WAITING"] = "joinWaiting";
+    WEB_SOCKET_EVENT["UPDATE"] = "update";
+    WEB_SOCKET_EVENT["PLAY"] = "play";
+    WEB_SOCKET_EVENT["CHAT"] = "chat";
+    WEB_SOCKET_EVENT["NEW_MESSAGE"] = "newMessage";
+    WEB_SOCKET_EVENT["PARTNER"] = "partner";
+})(WEB_SOCKET_EVENT = exports.WEB_SOCKET_EVENT || (exports.WEB_SOCKET_EVENT = {}));
